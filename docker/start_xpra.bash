78852ed9aeec47604553c874ca23f298018ff695
Tim Player <playert@oregonstate.edu>
2021-12-08 20:43:01 -0800

    Modify xpra script

--- docker/start_xpra.bash
+++ docker/start_xpra.bash
@@ -3,10 +3,10 @@
 # source this script if you need DISPLAY set `. start-xpra.sh`
 # $XPRASTART is intended for extra setting like `--dpi=108`
 
-if [ "$DISPLAY" = "" ]; then
-    export DISPLAY=:100
-fi
-if [ "$XPRAPORT" = "" ]; then
-    export XPRAPORT=6100
-fi
+export DISPLAY=:100
+echo "Set DISPLAY to $DISPLAY."
+export XPRAPORT=6100
+echo "Set XPRAPORT to $XPRAPORT."
+
+echo "Starting xpra with given DISPLAY=$DISPLAY and on port $XPRAPORT."
 xpra start $DISPLAY --bind-tcp=0.0.0.0:$XPRAPORT --pulseaudio=no --notifications=no --quality=99
