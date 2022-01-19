9efbbe922f5518be8837bdb347f3bdf5fb1ac6b6
Tim Player <timrplayer@gmail.com>
2022-01-19 12:16:34 -0800

    Add spawning and delay states

--- clgrasping_ws/src/cl_tsgrasp/nodes/states.py
+++ clgrasping_ws/src/cl_tsgrasp/nodes/states.py
@@ -2,21 +2,86 @@ import rospy
 from smach_ros import SimpleActionState
 import smach
 from franka_gripper.msg import MoveAction, MoveGoal, GraspAction, GraspGoal
-from geometry_msgs.msg import PoseStamped, Pose, Vector3
+from geometry_msgs.msg import PoseStamped, Pose, Vector3, Quaternion, Point
 import cl_tsgrasp.msg
 import numpy as np
 from rospy.numpy_msg import numpy_msg
 from utils import se3_dist
+from spawn_model import ObjectDataset
+from gazebo_msgs.srv import DeleteModel, SpawnModel
+import tf.transformations
 
 ## constants
 # GoToOrbitalPose
-ORBIT_RADIUS = 0.1 # distance to initially orbit object before terminal homing
+ORBIT_RADIUS   = 0.1 # distance to initially orbit object before terminal homing
 
-# Terminal Homing
-GOAL_PUB_RATE = 100.0
-STEP_SPEED = 1.0
-STEP_SIZE = STEP_SPEED / GOAL_PUB_RATE
-SUCCESS_RADIUS = 0.02 # in SE(3) space -- dimensionless
+# TerminalHoming
+GOAL_PUB_RATE   = 100.0
+STEP_SPEED      = 1.0
+STEP_SIZE       = STEP_SPEED / GOAL_PUB_RATE
+SUCCESS_RADIUS  = 0.02 # in SE(3) space -- dimensionless
+
+# SpawnNewItem
+WORKSPACE_BOUNDS    = [[-0.1, -0.3], [0.1, -0.1]]
+TABLE_HEIGHT        = 0.4
+
+# SpawnNewItem state
+class SpawnNewItem(smach.State):
+    # _cur_idx: int   = 0
+    # _cur_name: str  = None
+    _cur_idx = 0
+    _cur_name = None
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['spawned_new_item'])
+
+        self.obj_ds = ObjectDataset(
+            dataset_dir="/home/tim/Research/tsgrasp/data/dataset",
+            split="test"
+        )
+        self.delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
+        self.spawn_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
+        self._cur_idx = np.random.randint(0, len(self.obj_ds))
+
+    @staticmethod
+    def random_pose_in_workspace():
+        x = np.random.uniform(WORKSPACE_BOUNDS[0][0], WORKSPACE_BOUNDS[1][0])
+        y = np.random.uniform(WORKSPACE_BOUNDS[0][1], WORKSPACE_BOUNDS[1][1])
+
+        z = TABLE_HEIGHT
+        yaw = np.random.uniform(0, 2*np.pi)
+
+        x, y, yaw = 0, 0, 0 # test
+
+        q = tf.transformations.quaternion_from_euler(0, 0, yaw)
+        return Pose(
+            position=Point(x, y, z),
+            orientation=Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
+        )
+
+    def execute(self, userdata):
+        """ Delete old item, spawn new item. """
+        if self._cur_name is not None:
+            self.delete_model(self._cur_name)
+
+        if self._cur_idx > len(self.obj_ds): self._cur_idx = 0
+        obj = self.obj_ds[self._cur_idx]
+        self._cur_idx += 1
+        
+        self._cur_name = obj.name
+        item_pose = self.random_pose_in_workspace()
+        self.spawn_model(self._cur_name, obj.to_sdf(), "", item_pose, "world")
+        print(obj.to_sdf())
+        return 'spawned_new_item'
+
+class Delay(smach.State):
+    def __init__(self, seconds):
+        self.seconds = seconds
+        smach.State.__init__(self, outcomes=['delayed'])
+
+    def execute(self, userdata):
+        rospy.sleep(self.seconds)
+        return 'delayed'
 
 # GoToOrbitalPose state
 class GoToOrbitalPose(smach.State):
@@ -115,7 +180,7 @@ OpenJaws = SimpleActionState(
     goal=open_goal
 )
 
-    ## Close Jaws State
+## Close Jaws State
 close_goal = GraspGoal(
     width=0.00,
     speed=0.05, 
@@ -130,7 +195,13 @@ CloseJaws = SimpleActionState(
 )
 
 ## Reset Position State
-reset_pose = PoseStamped(pose=Pose(position=Vector3(z=0.7)))
+q = tf.transformations.quaternion_from_euler(np.pi, -np.pi/3, 0)
+reset_pose = PoseStamped(
+    pose=Pose(
+        position=Vector3(z=0.7),
+        orientation=Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
+    )
+)
 reset_pos_goal = cl_tsgrasp.msg.MotionGoal(goal_pose=reset_pose)
 ResetPos = SimpleActionState(
     '/motion_server',
