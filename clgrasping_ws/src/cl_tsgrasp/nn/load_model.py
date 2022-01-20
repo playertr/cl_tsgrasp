8ffe4562cdaa0330cd6c27443841cd69854162b5
Tim Player <timrplayer@gmail.com>
2022-01-19 23:21:35 -0800

    Update model

--- clgrasping_ws/src/cl_tsgrasp/nn/load_model.py
+++ clgrasping_ws/src/cl_tsgrasp/nn/load_model.py
@@ -11,22 +11,28 @@ import torch
 
 cfg_str = """
 training:
-  gpus: 1
+  gpus: 4
   batch_size: 3
   max_epochs: 100
   optimizer:
-    learning_rate: 0.0002
-    lr_decay: 0.95
-
+    learning_rate: 0.00025
+    lr_decay: 0.99
+  animate_outputs: false
+  make_sc_curve: false
+  use_wandb: false
+  wandb:
+    project: TSGrasp
+    experiment: tsgrasp_1_15
+    notes: Jan 15 run with all-frame loss, object frame, and object-category test/train.
 model:
   _target_: tsgrasp.net.lit_tsgraspnet.LitTSGraspNet
   model_cfg:
     backbone_model_name: MinkUNet14A
     D: 4
     backbone_out_dim: 128
-    add_s_loss_coeff: 100
+    add_s_loss_coeff: 10
     bce_loss_coeff: 1
-    width_loss_coeff: 10
+    width_loss_coeff: 1
     top_confidence_quantile: 1.0
     feature_dimension: 1
     pt_radius: 0.005
@@ -35,7 +41,7 @@ model:
     dilations:
     - 1 1 1 1
 
-ckpt_path: /home/tim/Research/cl_grasping/clgrasping_ws/src/cl_tsgrasp/nn/ckpts/tsgrasp_data_aug/model.ckpt
+ckpt_path: /home/tim/Research/cl_grasping/clgrasping_ws/src/cl_tsgrasp/nn/ckpts/tsgrasp_1_15/model.ckpt
 """
 
 def load_model():
