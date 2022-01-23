6571097ea3217510fa033b6bd716011a3c8ad344
Tim Player <timrplayer@gmail.com>
2022-01-23 09:48:40 -0800

    Add working MoveIt servo config

---
+++ clgrasping_ws/src/cl_tsgrasp/nodes/publish_twists.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3
+
+import rospy
+from std_msgs.msg import Header
+from geometry_msgs.msg import TwistStamped, Twist
+
+
+rospy.init_node('publish_tisists')
+twist_pub = rospy.Publisher('/servo_server/delta_twist_cmds', TwistStamped, queue_size=1)
+
+def publish_twist():
+    h = Header()
+    h.stamp = rospy.Time.now()
+    h.frame_id = 'panda_link0'
+
+    twist = Twist()
+    twist.linear.x = -0.6
+    twist.linear.y = 0.0
+    twist.linear.z = 0.0
+    twist.angular.x = 0.0
+    twist.angular.y = 0.0
+    twist.angular.z = 0.0
+
+    ts = TwistStamped(
+        header = h,
+        twist = twist
+    )
+
+    twist_pub.publish(ts)
+
+rospy.loginfo('Ready to publish twists.')\
+
+r = rospy.Rate(100)
+while not rospy.is_shutdown():
+    publish_twist()
+    r.sleep()
