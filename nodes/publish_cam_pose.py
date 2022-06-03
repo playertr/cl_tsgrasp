3818afebf9e2f61cd4da3abf6225dcb760e8f10f
Tim Player <playert@oregonstate.edu>
2022-06-03 13:51:55 -0700

    Change to TFHelper

--- nodes/publish_cam_pose.py
+++ nodes/publish_cam_pose.py
@@ -1,19 +1,22 @@
 #! /usr/bin/env python3
 
 import rospy
-from utils import TransformFrames
+from utils import TFHelper
 from geometry_msgs.msg import PoseStamped, Pose
 import tf2_ros
 
 
 rospy.init_node('publish_cam_pose')
 ee_pose_pub = rospy.Publisher('/tsgrasp/cam_pose', PoseStamped, queue_size=1)
-tf = TransformFrames()
+tf = TFHelper()
 
 def publish_cam_pose():
     while True:
         try:
-            cam_tf = tf.get_transform(source_frame="camera_depth_optical_frame", target_frame="world")
+            # TODO get frame name from rosparams
+            # cam_tf = tf.get_transform(source_frame="camera_depth_optical_frame", target_frame="world")
+
+            cam_tf = tf.get_transform(source_frame="kinect_optical_link", target_frame="world")
             break
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             rospy.sleep(1)
